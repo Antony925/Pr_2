c9a4a7303d354c9193a49878752008a6fce45748
Antony925 <152073152+Antony925@users.noreply.github.com>
2024-03-16 13:00:30 +0200

    Add files via upload

---
+++ MyBirthDatePicker.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KMA.ProgrammingInCSharp24
+{
+    /// <summary>
+    /// Логика взаимодействия для MyBirthDatePicker.xaml
+    /// </summary>
+    public partial class MyBirthDatePicker : Window
+    {
+        private string firstName;
+        private string lastName;
+        private string email;
+
+        public MyBirthDatePicker()
+        {
+            InitializeComponent();
+            LoaderIcon.Visibility = Visibility.Collapsed;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            firstName = FirstNameTextBox.Text;
+            lastName = LastNameTextBox.Text;
+            email = EmailTextBox.Text;
+            DateTime birthDate = BirthDatePicker.SelectedDate.GetValueOrDefault();
+
+            // Перевіряємо, чи всі поля введення не пусті
+            if (!string.IsNullOrWhiteSpace(FirstNameTextBox.Text) &&
+                !string.IsNullOrWhiteSpace(LastNameTextBox.Text) &&
+                !string.IsNullOrWhiteSpace(EmailTextBox.Text) &&
+                BirthDatePicker.SelectedDate != null)
+            {
+                ProceedButton.IsEnabled = true;
+            }
+            else
+            {
+                ProceedButton.IsEnabled = false;
+            }
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LoaderIcon.Visibility = Visibility.Visible;
+            DateTime selectedDate = BirthDatePicker.SelectedDate.GetValueOrDefault();
+
+            int age = Person.CalculateAge(selectedDate);
+            if (age < 0 || age > 135)
+            {
+                MessageBox.Show("Error! Please, enter your reall birth date!");
+                return;
+            }
+
+            if (Person.IsBirthDay(selectedDate))
+            {
+                MessageBox.Show("Happy Birthday to you!!!");
+            }
+
+            string w_Zodiac = Person.CalculateWesternZodiac(selectedDate);
+            string c_Zodiac = Person.CalculateChinaZodiac(selectedDate);
+
+
+            _Age.Text = $"You are {age} years old";
+            _Western.Text = $"Western zodiac: {w_Zodiac}";
+            _China.Text = $"China zodiac: {c_Zodiac}";
+            _FirstName.Text = $"First Name: {firstName}";
+            _LastName.Text = $"Last Name: {lastName}";
+            _Email.Text = $"Email: {email}";
+            _birthDate.Text = $"Birth Date: {selectedDate.ToString()}";
+            if (Person.IsBirthDay(selectedDate) == true)
+            {
+                _IsBirthDay.Text = "Today is your birthday";
+            }
+            else
+            {
+                _IsBirthDay.Text = "Today is not your birthday";
+            }
+            LoaderIcon.Visibility = Visibility.Collapsed;
+        }
+
+        public void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMA.ProgrammingInCSharp24
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public bool IsAdult
+        {
+            get
+            {
+                int age = CalculateAge(BirthDate);
+                return age >= 18;
+            }
+        }
+
+        public string WesternSign
+        {
+            get
+            {
+                return CalculateWesternZodiac(BirthDate);
+            }
+        }
+
+        public string ChineseSign
+        {
+            get
+            {
+                return CalculateChinaZodiac(BirthDate);
+            }
+        }
+
+        public bool IsBirthday
+        {
+            get
+            {
+                return IsBirthDay(BirthDate);
+            }
+        }
+
+        public Person(string firstName, string lastName, string emailAddress, DateTime birthDate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            EmailAddress = emailAddress;
+            BirthDate = birthDate;
+        }
+
+        public Person(string firstName, string lastName, string emailAddress)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            EmailAddress = emailAddress;
+            BirthDate = default; 
+        }
+
+        public Person(string firstName, string lastName, DateTime birthDate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            EmailAddress = string.Empty; 
+            BirthDate = birthDate;
+        }
+
+        public static int CalculateAge(DateTime birthDate)
+        {
+            DateTime currentDate = DateTime.Now;
+            int age = currentDate.Year - birthDate.Year;
+
+            if (currentDate.Month < birthDate.Month || (currentDate.Month == birthDate.Month && currentDate.Day < birthDate.Day))
+            {
+                age--;
+            }
+
+            return age;
+        }
+
+        public static string CalculateWesternZodiac(DateTime birthDate)
+        {
+            int month = birthDate.Month;
+            int day = birthDate.Day;
+            if ((month == 1 && day >= 21) || (month == 2 && day <= 19))
+                return "Водолій";
+            else if ((month == 2 && day >= 20) || (month == 3 && day <= 20))
+                return "Риби";
+            else if ((month == 3 && day >= 21) || (month == 4 && day <= 20))
+                return "Овен";
+            else if ((month == 4 && day >= 21) || (month == 5 && day <= 21))
+                return "Телець";
+            else if ((month == 5 && day >= 22) || (month == 6 && day <= 21))
+                return "Близнюки";
+            else if ((month == 6 && day >= 22) || (month == 7 && day <= 22))
+                return "Рак";
+            else if ((month == 7 && day >= 23) || (month == 8 && day <= 21))
+                return "Лев";
+            else if ((month == 8 && day >= 22) || (month == 9 && day <= 23))
+                return "Діва";
+            else if ((month == 9 && day >= 24) || (month == 10 && day <= 23))
+                return "Терези";
+            else if ((month == 10 && day >= 24) || (month == 11 && day <= 22))
+                return "Скорпіон";
+            else if ((month == 11 && day >= 23) || (month == 12 && day <= 22))
+                return "Стрілець";
+            else
+                return "Козоріг";
+        }
+
+        public static string CalculateChinaZodiac(DateTime birthDate)
+        {
+            int year = birthDate.Year;
+            string[] chineseZodiacs = { "Щур", "Бик", "Тигр", "Кролик", "Дракон", "Змія", "Кінь", "Коза", "Мавпа", "Півень", "Собака", "Свиня" };
+            int index = (year - 4) % 12;
+
+            if (index < 0)
+                index += 12;
+
+            return chineseZodiacs[index];
+        }
+
+        public static bool IsBirthDay(DateTime birthDate)
+        {
+            DateTime currentDate = DateTime.Now;
+            if (birthDate.Month == currentDate.Month && birthDate.Day == currentDate.Day)
+                return true;
+            return false;
+        }
+    }
+}
